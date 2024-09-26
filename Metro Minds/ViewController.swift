1c56f4940030ce0ee49da8dcacc7a0a96feeee88
Nikita <kaska0093@inbox.ru>
2024-09-26 01:59:55 -0700

    Initial Commit

---
+++ Metro Minds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Metro Minds
+//
+//  Created by Nikita Shestakov on 26.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
